b8605d0f9ca98ce18e8e2edd79b9e2e70cadfc75
Spencer Pollock <srepollock@gmail.com>
2016-09-12 22:15:13 -0700

    :panda_face: Initial Upload. So far the script is working. Included is a model I found online. Working to polish up controls and get everything working real nice and smooth now.

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public int minSpeed = 10;
+	public int maxSpeed = 100;
+	private int speedIncrease = 5;
+	public int currentSpeed = 10; // Initially is the slowest speed
+	public float rotateSpeed = 45.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		// Spacecraft Roll
+		if (Input.GetKey (KeyCode.A)) {
+			// Rotate about the X axis of the obj
+			transform.Rotate (0, 0, Time.deltaTime * rotateSpeed);
+		} else if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (0, 0, -Time.deltaTime * rotateSpeed);
+		}
+		// Spacecraft Acceleration/Deceleration
+		if (Input.GetKey (KeyCode.W)) {
+			currentSpeed += speedIncrease;
+			if (currentSpeed > maxSpeed)
+				currentSpeed = maxSpeed;
+		} else if (Input.GetKey (KeyCode.S)) {
+			currentSpeed -= speedIncrease;
+			if (currentSpeed < minSpeed)
+				currentSpeed = minSpeed;
+		}
+		// Move forward based to mouse
+		Vector3 mousePos = (Input.mousePosition - (new Vector3(Screen.width, Screen.height, 0) / 2.0f));
+		transform.Rotate (new Vector3 (-mousePos.y, mousePos.x, -mousePos.x) * 0.025f);
+		transform.Translate (Vector3.forward * Time.deltaTime * currentSpeed);
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Unity 3D Spaceship Scripts
+> This is just for testing and will be removed eventually
+This is the working script for a Spaceship using the mouse button as a movement control.
+- Speed up with `W`
+- Slow down with `S`
+- Tilt with `A` or `D`
