d224a74bf77a0d773885b0a5e144ae0a8b94ba14
Vincent Huang <poshenhuang123@hotmail.com>
2016-09-15 14:34:33 -0700

    Created inner circle of the torus. Gives warning when leaving bounds.

---
+++ Assets/Scripts/BoundaryCollision.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundaryCollision : MonoBehaviour {
+
+	private OOBscript oob;
+	public GameObject HUD;
+
+	// Use this for initialization
+	void Start () {
+		oob = HUD.GetComponent<OOBscript> ();
+		Debug.Log ("boundary created");
+	}
+
+	void OnTriggerStay(Collider other) {
+		oob = HUD.GetComponent<OOBscript> ();
+		oob.danger = true;
+		//Debug.Log ("Object inside");
+	}
+
+	void OnTriggerExit(Collider other) {
+		oob = HUD.GetComponent<OOBscript> ();
+		oob.danger = false;
+		Debug.Log ("Object exited");
+	}
+
+	void OnTriggerEnter(Collider other) {
+		oob = HUD.GetComponent<OOBscript> ();
+		oob.danger = true;
+		Debug.Log ("Object entered");
+	}
+}
