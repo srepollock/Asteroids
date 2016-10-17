c0f6fb6acdcc92138f73ef1b8a24c000950ccd5c
Hank Lo <Hlo1453@gmail.com>
2016-10-17 12:40:30 -0700

    added shot deviation

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -12,9 +12,20 @@ public class PlayerControls : MonoBehaviour {
 	public MenuManager menuManager;
     public GameObject shot;
     public Transform shotSpawn;
-    public float fireRate = 0.5f;
+    public float fireRate = 0.01f;
 
+    // shot deviation variables
+    public float deviationIncreaseAmount = 0.1f;
+    public float deviationDecreaseAmount = 0.1f;
+    private float shotDeviationScale = 1;
+    private Time mouseHeldDown;
+    private Time mouseUp;
+    private bool shooting;
     private float nextFire = 0.0f;
+    private float bulletStrayX = 0;
+    private float bulletStrayY = 0;
+    private float bulletStrayZ = 0;
+
 	private Rect deadzone;
 
 	// Use this for initialization
@@ -27,9 +38,38 @@ public class PlayerControls : MonoBehaviour {
 	}
 	
     void Update() {
-        if (Input.GetButton("Fire1") && Time.time > nextFire) {
+        if ((Input.GetButton("Fire1")) && (Time.time > nextFire)) {
             nextFire = Time.time + fireRate;
-            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GameObject newshot = (GameObject) Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+
+            // first shot is perfect straight shot, others will deviate
+            if (shooting) {
+            	// generate random numbers for stray
+	            bulletStrayX = Random.Range((-1 * shotDeviationScale), (1 * shotDeviationScale));
+	            bulletStrayY = Random.Range((-1 * shotDeviationScale), (1 * shotDeviationScale));
+	            bulletStrayZ = Random.Range((-1 * shotDeviationScale), (1 * shotDeviationScale));
+
+	            newshot.transform.Rotate(bulletStrayX, bulletStrayY, bulletStrayZ);
+            }
+
+            shooting = true;
+        }
+
+        if (Input.GetButtonUp("Fire1")) {
+        	shooting = false;
+        }
+
+        if (shooting) {
+        	// increase shot deviation
+        	shotDeviationScale += deviationIncreaseAmount;
+        	if (shotDeviationScale > 5f) {
+        		shotDeviationScale = 5;
+        	}
+        } else {
+        	shotDeviationScale -= deviationDecreaseAmount;
+        	if (shotDeviationScale < 1) {
+        		shotDeviationScale = 1;
+        	}
         }
     }
 
