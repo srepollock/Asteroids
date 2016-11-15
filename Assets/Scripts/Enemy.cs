14dfe37ea12000a29e054a4f88f4af8bc9ee120e
Hank Lo <Hlo1453@gmail.com>
2016-11-14 22:01:01 -0800

    More enemy boss setup, as well as started setting up functions for movement.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,14 +2,83 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
+	// 0 is scout type, 1 is cruiser type, 2 is mothership type
+	public int type;
+	public int hp;
+
+	public int aggroState;
 
 	// Use this for initialization
 	void Start () {
-	
+		if (type == 0) {
+			hp = 500;
+		}
+		if (type == 1) {
+			hp = 50000;
+		}
+		if (type == 2) {
+			hp = 1000000;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+			
+	}
+
+	public void UpwardsDodgeManeuver() {
+
+	}
+
+	public void CircularDodging() {
+
+	}
+
+	public void ForwardsThrust() {
+
+	}
+
+	public void ReverseThrust() {
+
+	}
+
+	public void LeftThrust() {
+
+	}
+
+	public void RightThrust() {
+
+	}
+
+	public void PitchUp() {
+
+	}
+
+	public void PitchDown() {
+
+	}
+
+	public void YawRight() {
+
+	}
+
+	public void YawLeft() {
+
+	}
+
+	public void RollLeft() {
+
+	}
+
+	public void RollRight() {
+
+	}
+
+	public void HopUp() {
+
+	}
+
+	public void HopDown() {
+
 	}
 }
