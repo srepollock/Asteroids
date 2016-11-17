cc849e3e5cb9dee41077363b5da4a3d699f2f1b7
Matthew Wong <matthewwong3.14@gmail.com>
2016-11-17 15:47:14 -0800

    Pausing feature implemented. Press 'p' to pause. From pause menu, you can unpause by pressing 'p' or clicking on the return to game button. From the pause menu, you can quit to the main menu.

---
+++ Assets/Scripts/PauseGameScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGameScript : MonoBehaviour {
+
+    private bool isPaused = false;
+    public GameObject pauseMenuCanvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isPaused = !isPaused;
+
+            if (isPaused)
+            {
+                pauseGame();
+            } else
+            {
+                unpauseGame();
+            }
+        }
+	}
+
+    public void pauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0.0f;
+        pauseMenuCanvas.SetActive(true);
+    }
+
+    public void unpauseGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1.0f;
+        pauseMenuCanvas.SetActive(false);
+    }
+}
