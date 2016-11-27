caaa1f8d675b201e6fde965c71abc3104b711692
Spencer Pollock <srepollock@gmail.com>
2016-11-27 14:08:57 -0800

    Asteroid bullet collision
    All asteroid objects need to have the tag "Asteroids" to remove the bullets

--- Assets/Scripts/PlayerShot.cs
+++ Assets/Scripts/PlayerShot.cs
@@ -5,4 +5,10 @@ public class PlayerShot : MonoBehaviour {
 
 	public static int SHOTDAMAGE = 50;
 
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.tag == "Asteroid") {
+            Destroy(this);
+        }
+    }
+
 }
