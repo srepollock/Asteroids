fa483534e2f00689138fa8e2d41fe83a3bb564c3
Aify <hlo1453@gmail.com>
2016-11-28 18:00:32 -0800

    Boosting implemented for all 3 prefabs

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -18,6 +18,14 @@ public class PlayerControls : MonoBehaviour {
     public float deviationDecreaseAmount = 0.1f;
     public Slider speedSlider;
 
+    // boost stuff
+    public int maxBoost = 100;
+    public int boostSpeed = 100;
+    public int curBoost = 0;
+    public int boostCd = 0;
+    public int boostCdCur = 0;
+    public bool boosting = false;
+
     private float shotDeviationScale = 1;
     private Time mouseHeldDown;
     private Time mouseUp;
@@ -43,79 +51,99 @@ public class PlayerControls : MonoBehaviour {
 	
     void FixedUpdate() {
         if (Time.timeScale == 1.0f) {
-            if (Input.GetKey(KeyCode.Q)) {
-                transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
-            } else if (Input.GetKey(KeyCode.E)) {
-                transform.Rotate(0, 0, rotateSpeed * Time.deltaTime * -1f);
-            } if (slowingDown) {
-                ModifySpeed(-speedIncrease * 5);
-                if (currentSpeed == minSpeed) {
-                    currentSpeed = 0;
-                    menuManager.increaseLevel();
+
+            if ((Input.GetKey(KeyCode.LeftControl)) && (boostCdCur <= 0) && (!boosting)) {
+                boosting = true;
+                curBoost = 0;
+            }
+
+            // direction locked in while boosting
+            if (!boosting) {
+                if (Input.GetKey(KeyCode.Q)) {
+                    transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+                } else if (Input.GetKey(KeyCode.E)) {
+                    transform.Rotate(0, 0, rotateSpeed * Time.deltaTime * -1f);
+                } if (slowingDown) {
+                    ModifySpeed(-speedIncrease * 5);
+                    if (currentSpeed == minSpeed) {
+                        currentSpeed = 0;
+                        menuManager.increaseLevel();
+                    }
+                } else {
+                    // Spacecraft Acceleration/Deceleration (THRUST)
+                    if (Input.GetKey(KeyCode.W)) {
+                        ModifySpeed(speedIncrease);
+                    }
+                    else if (Input.GetKey(KeyCode.S)) {
+                        ModifySpeed(-speedIncrease);
+                    }
                 }
+
+                // strafing and up/down is based off thrust
+                if (Input.GetKey(KeyCode.A)) {  
+                    transform.Translate(Vector3.right * -1 * Time.deltaTime * currentSpeed/5);
+                } else if (Input.GetKey(KeyCode.D)) {
+                    transform.Translate(Vector3.right * Time.deltaTime * currentSpeed/5);
+                }
+
+                if (Input.GetKey(KeyCode.Space)) {
+                    transform.Translate(Vector3.up * Time.deltaTime * currentSpeed/5);
+                } else if (Input.GetKey(KeyCode.LeftShift)) {
+                    transform.Translate(Vector3.up * -1 * Time.deltaTime * currentSpeed/5);
+                }
+
+                Vector3 mousePos = (Input.mousePosition - (new Vector3(Screen.width, Screen.height, 0) / 2.0f));
+                if (deadzone.Contains(Input.mousePosition)) {
+                    mousePos = Vector3.zero;
+                }
+                transform.Rotate(new Vector3(-mousePos.y, mousePos.x, -mousePos.x) * rotateSpeed * Time.deltaTime * 0.005f);
+                transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed);
+
+                boostCdCur--;
             } else {
-                // Spacecraft Acceleration/Deceleration (THRUST)
-                if (Input.GetKey(KeyCode.W)) {
-                    ModifySpeed(speedIncrease);
+                transform.Translate(Vector3.forward * Time.deltaTime * (maxSpeed + boostSpeed));
+
+                curBoost++;
+                if (curBoost >=maxBoost) {
+                    boosting = false;
+                    boostCdCur = boostCd;
                 }
-                else if (Input.GetKey(KeyCode.S)) {
-                    ModifySpeed(-speedIncrease);
-                }
             }
-
-            // strafing and up/down is based off thrust
-            if (Input.GetKey(KeyCode.A)) {  
-                transform.Translate(Vector3.right * -1 * Time.deltaTime * currentSpeed/5);
-            } else if (Input.GetKey(KeyCode.D)) {
-                transform.Translate(Vector3.right * Time.deltaTime * currentSpeed/5);
-            }
-
-            if (Input.GetKey(KeyCode.Space)) {
-                transform.Translate(Vector3.up * Time.deltaTime * currentSpeed/5);
-            } else if (Input.GetKey(KeyCode.LeftShift)) {
-                transform.Translate(Vector3.up * -1 * Time.deltaTime * currentSpeed/5);
-            }
-
-            
-            
-            Vector3 mousePos = (Input.mousePosition - (new Vector3(Screen.width, Screen.height, 0) / 2.0f));
-            if (deadzone.Contains(Input.mousePosition)) {
-                mousePos = Vector3.zero;
-            }
-            transform.Rotate(new Vector3(-mousePos.y, mousePos.x, -mousePos.x) * rotateSpeed * Time.deltaTime * 0.005f);
-            transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed);
         }
     }
 
     void Update() {
         if (Time.timeScale == 1) {
-            if ((Input.GetButton("Fire1")) && (Time.time > nextFire)) {
-				laserBlast.Play ();
-                nextFire = Time.time + fireRate;
-                GameObject newshot = (GameObject)Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            // cant shoot while boosting
+                if (!boosting) {
+                if ((Input.GetButton("Fire1")) && (Time.time > nextFire)) {
+    				laserBlast.Play ();
+                    nextFire = Time.time + fireRate;
+                    GameObject newshot = (GameObject)Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+                    if (shooting) {
+                        // generate random numbers for stray
+                        bulletStrayX = Random.Range((-1 * shotDeviationScale), (1 * shotDeviationScale));
+                        bulletStrayY = Random.Range((-1 * shotDeviationScale), (1 * shotDeviationScale));
+                        bulletStrayZ = Random.Range((-1 * shotDeviationScale), (1 * shotDeviationScale));
+                        newshot.transform.Rotate(bulletStrayX, bulletStrayY, bulletStrayZ);
+                    }
+                    shooting = true;
+                }
+                if (Input.GetButtonUp("Fire1")) {
+                    shooting = false;
+                }
                 if (shooting) {
-                    // generate random numbers for stray
-                    bulletStrayX = Random.Range((-1 * shotDeviationScale), (1 * shotDeviationScale));
-                    bulletStrayY = Random.Range((-1 * shotDeviationScale), (1 * shotDeviationScale));
-                    bulletStrayZ = Random.Range((-1 * shotDeviationScale), (1 * shotDeviationScale));
-                    newshot.transform.Rotate(bulletStrayX, bulletStrayY, bulletStrayZ);
+                    shotDeviationScale += deviationIncreaseAmount;
+                    if (shotDeviationScale > 5f) {
+                        shotDeviationScale = 5;
+                    }
                 }
-                shooting = true;
-            }
-            if (Input.GetButtonUp("Fire1")) {
-                shooting = false;
-            }
-            if (shooting) {
-                shotDeviationScale += deviationIncreaseAmount;
-                if (shotDeviationScale > 5f) {
-                    shotDeviationScale = 5;
-                }
-            }
-            else {
-                shotDeviationScale -= deviationDecreaseAmount;
-                if (shotDeviationScale < 1)
-                {
-                    shotDeviationScale = 1;
+                else {
+                    shotDeviationScale -= deviationDecreaseAmount;
+                    if (shotDeviationScale < 1)
+                    {
+                        shotDeviationScale = 1;
+                    }
                 }
             }
         }
